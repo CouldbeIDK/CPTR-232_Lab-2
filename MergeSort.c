56c7c0ffa664ab8216b62cbea8634a563336f20d
William Brownson <Williamiv95@gmail.com>
2014-10-08 02:46:45 -0700

    Welp
    Merge sort... works... sorta... well it compiles anyway

--- MergeSort.c
+++ MergeSort.c
@@ -1,7 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-void MERGE(A,p,q,r)
+void MERGE(int *A,int p,int q,int r){
+	int na = q - p + 1;
+	int nb = r - q;
+	int L[na], R[nb], i, j, k;
+	for(i = 0; i < na ; i++){
+		L[i] = A[p+i-1];
+	}
+	for(j = 0; j < nb ; j++){
+		R[j] = A[q+j];
+	}
+	i = 0;
+	j = 0;
+	k = 0;
+	for (k = p; k <= r ; k++){
+		if ((L[i] <= R[j]) && (i < na)){
+			A[k] = L[i];
+			i++;
+		}else{
+			A[k] = R[j];
+			j++;
+		}
+	}
+}
+
+void MERGESORT(int *A, int p, int r){
+	if (p < r) {
+		int q = (p + r)/2;
+		MERGESORT(A,p,q);
+		MERGESORT(A,q + 1, r);
+		MERGE(A,p,q,r);
+	}
+}
 
 int main() {
 	int sorted[10000];
@@ -11,6 +43,8 @@ int main() {
 		sorted[i++] = j;
 	}
 	slength = i;
+	
+	MERGE(sorted, 0, slength / 2, slength);
 	
 	for (k=0 ; k < slength ; k++){
 		printf("%d\n", sorted[k]);
